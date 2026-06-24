5fa405faddcdc0301dd1938c8ce45f3ae2326c38
lemio <geert.roumen@gmail.com>
2026-06-24 14:37:58 +0200

    Added web assembly

---
+++ build-wasm/src/loom/_config.h
@@ -0,0 +1,11 @@
+// Copyright 2016
+// Author: Patrick Brosi
+
+#ifndef LOOM_CONFIG_H_
+#define LOOM_CONFIG_H_
+
+
+// version number from cmake version module
+#define VERSION_FULL "-128-NOTFOUND-dirty"
+
+#endif  // LOOM_CONFIG_H_N
